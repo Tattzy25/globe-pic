e6c60aa63c76c297558045d10abdf7109eb0ef7b
tattzy25 <tattzy2025@gmail.com.com>
2026-02-17 01:23:15 -0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import DomeGallery from "../components/DomeGallery";
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <DomeGallery
+        fit={0.8}
+        minRadius={600}
+        maxVerticalRotationDeg={0}
+        segments={34}
+        dragDampening={2}
+        grayscale
+      />
+    </div>
+  );
+}
